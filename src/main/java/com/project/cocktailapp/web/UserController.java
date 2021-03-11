ca1f08f21ebf38052736ea1830e354286d5fdbb0
Vasil <vvpanayotovv@gmail.com>
2021-03-11 16:32:13 +0200

    add simple controllers users and security

---
+++ src/main/java/com/project/cocktailapp/web/UserController.java
@@ -0,0 +1,20 @@
+package com.project.cocktailapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
