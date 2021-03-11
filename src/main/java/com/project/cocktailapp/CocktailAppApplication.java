882de99eae86a5f06ad21a7a8a209e030c7837f7
Vasil <vvpanayotovv@gmail.com>
2021-03-11 14:33:52 +0200

    initial commit

---
+++ src/main/java/com/project/cocktailapp/CocktailAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.cocktailapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocktailAppApplication.class, args);
+    }
+
+}
