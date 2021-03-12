1804c87e999d3194db1c03654a1a1a0c16aa759b
Vasil <vvpanayotovv@gmail.com>
2021-03-12 14:37:37 +0200

    change user entity update views

--- src/main/java/com/project/cocktailapp/web/CocktailController.java
+++ src/main/java/com/project/cocktailapp/web/CocktailController.java
@@ -2,11 +2,13 @@ package com.project.cocktailapp.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/cocktails")
 public class CocktailController {
 
-    @GetMapping("/cocktails")
+    @GetMapping("/all")
     public String allCocktails(){
         return "all-cocktails";
     }
