e97a922681c1a83ca1f3466778b03357396a7c9a
Vasil <vvpanayotovv@gmail.com>
2021-03-22 18:55:36 +0200

    implement login / logout

--- src/main/java/com/project/cocktailapp/web/UserController.java
+++ src/main/java/com/project/cocktailapp/web/UserController.java
@@ -1,9 +1,4 @@
 package com.project.cocktailapp.web;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
