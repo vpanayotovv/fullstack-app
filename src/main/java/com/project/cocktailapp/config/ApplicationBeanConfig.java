4ce1681eb168cdc6031e4516252ab58e03c042fc
Vasil <vvpanayotovv@gmail.com>
2021-04-19 19:37:51 +0300

    up

--- src/main/java/com/project/cocktailapp/config/ApplicationBeanConfig.java
+++ src/main/java/com/project/cocktailapp/config/ApplicationBeanConfig.java
@@ -7,11 +7,12 @@ import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.web.client.RestTemplate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -40,5 +41,10 @@ public class ApplicationBeanConfig {
                 .setPrettyPrinting()
                 .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, type, context) -> LocalDateTime.parse(json.getAsString(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
                 .create();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
     }
 }
